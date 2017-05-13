965bcf90e6a9c74f2de64ea06702f57e2afde100
gjandsh <gjandsh@126.com>
2017-05-13 11:24:39 +0800

    添加回到顶部和缓慢滚动

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
 
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+})
+$('#gototop').on('click',function(){
+        $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+})
